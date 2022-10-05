backpropogation error-fixed

ClassFiles/Framework.py
@@ -1,7 +1,7 @@
-#import nntplib
+import nntplib
 import numpy as np
 import os
-#import odl
+import odl
 from abc import ABC, abstractmethod
 from ClassFiles import util as ut
 #import tensorflow as tf
@@ -223,11 +223,11 @@ class AdversarialRegulariser(GenericFramework):
 
             data_error, was_output, cut_reco, quality = self.calculate_pic_grad(reconstruction=guess, data_term=y, mu=mu, ground_truth=x_true)
             
-            writer.add_scalar('Data_Loss', data_error.detach().cpu().numpy(), k)
-            writer.add_scalar('Wasserstein_Loss', was_output.detach().cpu().numpy(), k)
-            writer.add_scalar('L2_to_ground_truth', quality.detach().cpu().numpy(), k)
-            writer.add_image('Reconstruction', cut_reco.detach().cpu().numpy()[0])
-            writer.add_image('Ground_truth', x_true[0])
+            writer.add_scalar('Picture_Optimization/Data_Loss', data_error.detach().cpu().numpy(), k)
+            writer.add_scalar('Picture_Optimization/Wasserstein_Loss', was_output.detach().cpu().numpy(), k)
+            writer.add_scalar('Picture_Optimization/L2_to_ground_truth', quality.detach().cpu().numpy(), k)
+            writer.add_image('Picture_Optimization/Reconstruction', cut_reco.detach().cpu().numpy()[0])
+            writer.add_image('Picture_Optimization/Ground_truth', x_true[0])
             guess = self.update_pic(1, step_s, y, guess, mu)
             del data_error, was_output, cut_reco, quality
             #torch.cuda.empty_cache()
@@ -237,17 +237,14 @@ class AdversarialRegulariser(GenericFramework):
 
         # the network outputs
         gen_im = torch.tensor(gen_im, requires_grad=True, device=self.device)
-        true_im = torch.tensor(true_im, requires_grad=True, device=self.device)
+        true_im = torch.tensor(true_im, requires_grad=False, device=self.device)
         random_uint = torch.tensor(random_uint, device=self.device)
         gen_was = self.network(gen_im)
         data_was = self.network(true_im)
 
         # Wasserstein loss
         wasserstein_loss = torch.mean(data_was-gen_was)
-        print('Data Wass.:')
-        print(torch.mean(data_was).item())
-        # print('Gen Wass.:')
-        # print(torch.mean(gen_was))
+        
         # intermediate point
         random_uint_exp = torch.unsqueeze(torch.unsqueeze(
             torch.unsqueeze(random_uint, axis=1), axis=1), axis=1)
@@ -257,7 +254,7 @@ class AdversarialRegulariser(GenericFramework):
 
         # calculate derivative at intermediate point
         gradient_was = torch.autograd.grad(
-            inter_was, inter, grad_outputs=torch.ones_like(inter_was), create_graph= True, retain_graph= True)[0]
+            inter_was, inter, grad_outputs=torch.ones_like(inter_was), create_graph=True, retain_graph=True)[0]
         
         # take the L2 norm of that derivative
         norm_gradient = torch.sqrt(torch.sum(
@@ -288,10 +285,6 @@ class AdversarialRegulariser(GenericFramework):
             # optimize network
             loss, wasserstein_loss, regulariser_was = self.train_step(gen_im=guess, true_im=x_true, random_uint=epsilon)
             loss.backward()
-            print('Netzwerk:')
-            print(loss.item())
-            print('Wasserstein:')
-            print(wasserstein_loss)
             self.optimizer.step()
             del loss, wasserstein_loss, regulariser_was
             #torch.cuda.empty_cache()
@@ -322,10 +315,11 @@ class AdversarialRegulariser(GenericFramework):
             # get the batch size - all gradients have to be scaled by the batch size as they are taken over previously
             # averaged quantities already. Makes gradients scaling batch size inveriant
             batch_s = reconstruction.size()[0]
+            full_error = full_error * batch_s
 
             # Optimization for the picture
             pic_grad = torch.autograd.grad(
-                full_error * batch_s, reconstruction)
+                full_error, reconstruction)
             pic_grad_cpu = pic_grad[0].detach().cpu().numpy()
             del reconstruction, data_term, data_mismatch, data_error, full_error, pic_grad, mu, ray, batch_s
             #torch.cuda.empty_cache()

ClassFiles/networks.py
@@ -41,39 +41,48 @@ class ConvNetClassifier(nn.Module):
         self.colors = colors
         self.conv1 = nn.Conv2d(3,16,5,padding="same")
         self.conv2 = nn.Conv2d(16,32,5,padding="same")
-        self.conv3 = Conv2dSame(32,32,5, stride = 2)
+        self.conv3 = Conv2dSame(32,32,5,stride=2)
         self.conv4 = Conv2dSame(32,64,5, stride=2)
         self.conv5 = Conv2dSame(64,64,5, stride=2)
         self.conv6 = Conv2dSame(64,128,5, stride=2)
-        #image size is now imagesize/16
-
+        
         # reshape for classification - assumes image size is multiple of 32
-        finishing_size = int(self.size[-2] * self.size[-1]/(16*16))
+        finishing_size = int(self.size[0] * self.size[1]/(16*16))
         self.dimensionality = finishing_size * 128
         #reshaped = tf.reshape(self.conv6, [-1, dimensionality])
         
         self.fc1 = nn.Linear(self.dimensionality, 256)
         self.fc2 = nn.Linear(256, 1)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            print(m)
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.xavier_uniform(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+        
 
     def forward(self, x):
         
         x = x.type(torch.FloatTensor)
         x = x.to(self.device)
         x = self.conv1(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x) #negative_slope = 0.2 to be comparable with tensorflow implementation
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         x = self.conv2(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         x = self.conv3(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         x = self.conv4(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         x = self.conv5(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         x = self.conv6(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
-        x = torch.flatten(x, start_dim=1)    
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
+        x = torch.reshape(x, (-1,self.dimensionality))    
         x = self.fc1(x)
-        x = nn.LeakyReLU(negative_slope=0.2)(x)
+        x = nn.LeakyReLU(negative_slope=0.3)(x)
         output = self.fc2(x)
 
         return output        
25a884186a6a4180756ce3276ee3db8cc05549b7
Samira Kabri <samira.kabri@fau.de>
2023-05-15 14:54:11 +0200

    minor changes

--- Resolution_Experiments.py
+++ Resolution_Experiments.py
@@ -17,9 +17,9 @@ import fourierimaging as fi
 
 DATA_PATH = '../Data/data/images/'
 SAVES_PATH = '../Saves/'
-fname = 'fno_nostride_k1'
+fname = 'fno_nostride'
 
-resolutions = [(128,128)]
+resolutions = [(64,64),(128,128), (256,256)]
 
 
 
@@ -29,22 +29,23 @@ resolutions = [(128,128)]
 for res in resolutions:
     Adversarial_Regulariser.fix_seed(42)
     experiment = Adversarial_Regulariser.Experiment1(DATA_PATH, SAVES_PATH, image_size = res, exp_name= fname)
-    experiment.noise_level = 0.05
+    experiment.noise_level = 0.05*res[0]/64
 
     experiment.data_clipping = 'Clipped_data'
     lmb = experiment.find_good_lambda(32)
     experiment.mu_default = lmb
 
     Adversarial_Regulariser.fix_seed(42)
-    # datapip = experiment.get_Data_pip(DATA_PATH, experiment.image_size)
-    # img = datapip.load_data()
+    datapip = experiment.get_Data_pip(DATA_PATH, experiment.image_size)
+    img = datapip.load_data()
 
-    # noisy_img = experiment.generate_training_data(batch_size = 1)[0]
-    # plt.figure()
-    # plt.imshow(img[:,:,0])
-    # plt.imshow(noisy_img[0,0])
+    noisy_img = experiment.generate_training_data(batch_size = 1)[0]
+    plt.figure()
+    plt.imshow(img[:,:,0])
+    plt.figure()
+    plt.imshow(noisy_img[0,0])
 
-    experiment.log_optimization(batch_size=1, steps=200, step_s=0.01,mu=lmb)
+    #experiment.log_optimization(batch_size=1, steps=200, step_s=0.01,mu=lmb)
 # %%
 
 net = experiment.get_network(size=None, colors=1)
